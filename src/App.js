481dfa37f225cb18f6958d66c764f55fc6a2e080
Roshan sharma <roshansharma406@gmail.com>
2020-09-17 19:57:21 +0530

    Axios added

--- src/App.js
+++ src/App.js
@@ -7,6 +7,13 @@ import Checkout from './components/Checkout';
 import Login from './components/Login';
 import { auth } from './firebase';
 import { useStateValue } from './StateProvider';
+import Payment from './components/Payment';
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
+
+const promise = loadStripe(
+  'pk_test_51HQT5MAGLKsUolpmmq3yd7oU4WJZNJVrJhXS3HALkVo1HueI7GxEUsumiAVv1cKMRGWy1XHCBGBJgKgJLYhpQLvi00SFZd93Ct'
+);
 
 function App() {
   const [{}, dispatch] = useStateValue();
@@ -14,7 +21,7 @@ function App() {
   useEffect(() => {
     // will run only once when app component loads
     auth.onAuthStateChanged((authUser) => {
-      console.log('USER IS >>>>', authUser);
+      // console.log('USER IS >>>>', authUser);
       if (authUser) {
         // user just logged in / the user was logged in
         dispatch({
@@ -43,6 +50,13 @@ function App() {
           <Route path='/checkout'>
             <Header />
             <Checkout />
+          </Route>
+
+          <Route path='/payment'>
+            <Header />
+            <Elements stripe={promise}>
+              <Payment />
+            </Elements>
           </Route>
 
           <Route path='/'>
--- src/components/Subtotal.js
+++ src/components/Subtotal.js
@@ -2,9 +2,11 @@ import React from 'react';
 import './Subtotal.css';
 import CurrencyFormat from 'react-currency-format';
 import { useStateValue } from '../StateProvider';
-import { getbasketTotal } from '../reducer';
+import { getBasketTotal } from '../reducer';
+import { useHistory } from 'react-router-dom';
 
 function Subtotal() {
+  const history = useHistory();
   const [{ basket }, dispatch] = useStateValue();
 
   return (
@@ -22,12 +24,14 @@ function Subtotal() {
           </>
         )}
         decimalScale={2}
-        value={getbasketTotal(basket)}
+        value={getBasketTotal(basket)}
         displayType={'text'}
         thousandSeparator={true}
         prefix={'$'}
       />
-      <button>Proceed to Checkout</button>
+      <button onClick={(e) => history.push('/payment')}>
+        Proceed to Checkout
+      </button>
     </div>
   );
 }
